5b2d303f4562976ef66f6e35775fb97381a380b4
barisbulutdemir <barisbulutdemir@gmail.com>
2023-08-06 16:29:33 +0200

    s
    s

--- auth/settings.py
+++ auth/settings.py
@@ -157,7 +157,9 @@ else:
     AWS_S3_REGION_NAME = getenv('AWS_S3_REGION_NAME')
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_LOCATION = 'static'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400'
